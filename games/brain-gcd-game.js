50eb8e7631cc750044cfc28b2f532bac5c088408
Ilia Loginov <loginoviv43@gmail.com>
2022-06-13 19:23:45 +0300

    add brain-gcd game

---
+++ games/brain-gcd-game.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+import randomNumb from '../src/random.js';
+
+const questionFn = () => {
+  console.log('Find the greatest common divisor of given numbers.');
+};
+
+const dividers = (number) => {
+  const resultArray = [];
+  for (let i = 1; i <= number; i += 1) {
+    if (number % i === 0) {
+      resultArray.push(i);
+    }
+  }
+  return resultArray;
+};
+
+const generateRound = () => {
+  const num1 = randomNumb();
+  const num2 = randomNumb();
+  const question = [num1, num2].join(' ');
+  const dividersNum1 = dividers(num1);
+  const dividersNum2 = dividers(num2);
+
+  const answerArray = [];
+  for (const elem of dividersNum1) {
+    if (dividersNum2.includes(elem)) {
+      answerArray.push(elem);
+    }
+  }
+  const answer = String(answerArray[answerArray.length - 1]);
+  return [question, answer];
+};
+
+export { questionFn, generateRound };
