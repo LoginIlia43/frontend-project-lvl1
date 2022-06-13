d70c5e82487df85e59188a2ca737ff41faaa4388
Ilia Loginov <loginoviv43@gmail.com>
2022-06-13 20:44:06 +0300

    lint brain-progression-game.js

--- games/brain-progression-game.js
+++ games/brain-progression-game.js
@@ -1,31 +1,29 @@
 #!/usr/bin/env node
-import randomNumb from '../src/random.js';
 import lodash from 'lodash';
+import randomNumb from '../src/random.js';
 
 const questionFn = () => {
   console.log('What number is missing in the progression?');
 };
-
-
 
 const generateRound = () => {
   const startNumber = randomNumb();
   const progressionStep = randomNumb();
   const progression = [startNumber];
 
-  //Generate a progression
+  // Generate a progression
   let progNumber = startNumber;
   for (let i = 1; i < 10; i += 1) {
     progNumber += progressionStep;
     progression.push(progNumber);
   }
 
-  //Hide the element
+  // Hide the element
   const numHideElem = lodash.random(0, 9);
   const hiddenElement = String(progression[numHideElem]);
   progression[numHideElem] = '..';
 
-  return [progression, hiddenElement]
-}
+  return [progression, hiddenElement];
+};
 
 export { questionFn, generateRound };
