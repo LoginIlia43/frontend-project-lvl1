5486f6182b334d3e360d09b65c54570fe9829c7b
Ilia Loginov <loginoviv43@gmail.com>
2022-06-13 17:45:00 +0300

    correct brain-even game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,38 +1,6 @@
 #!/usr/bin/env node
-import lodash from 'lodash';
-import readlineSync from 'readline-sync';
-import userName from '../src/cli.js';
+import helloFn from '../src/hello.js';
+import { questionFn, generateRound } from '../games/brain-even-game.js';
+import gameFunc from '../src/index.js';
 
-console.log('Welcome to the Brain Games!');
-const name = userName();
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-const isEven = (number) => {
-  if (number % 2 === 0) {
-    return 'yes';
-  }
-  return 'no';
-};
-
-const evenGame = () => {
-  let countAnswers = 0;
-
-  for (let i = 0; i < 3; i += 1) {
-    const randNumb = lodash.random(0, 100);
-    const playerAnswer = readlineSync.question(`Question: ${randNumb}\nYour answer: `);
-    const correctAnswer = isEven(randNumb);
-
-    if (playerAnswer !== correctAnswer) {
-      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
-      break;
-    } else {
-      console.log('Correct!');
-      countAnswers += 1;
-    }
-  }
-  if (countAnswers === 3) {
-    console.log('Congratulations!');
-  }
-};
-
-evenGame();
+gameFunc(helloFn, questionFn, generateRound);
---
+++ games/brain-even-game.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import randomNumb from '../src/random.js';
+
+const questionFn = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+};
+
+const isEven = (number) => {
+  if (number % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
+};
+
+const generateRound = () => {
+  const question = randomNumb();
+  const answer = isEven(question);
+return [question, answer];
+};
+
+export { questionFn, generateRound };
