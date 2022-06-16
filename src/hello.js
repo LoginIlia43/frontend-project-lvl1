841ac328db40542e313d765df6529c1dff78f2ef
Ilia Loginov <loginoviv43@gmail.com>
2022-06-16 17:59:12 +0300

    delete hello.js

--- src/hello.js
+++
@@ -1,9 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const helloFn = () => {
-  const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
-  console.log(`Hello, ${name}!`);
-  return name;
-};
-
-export default helloFn;
